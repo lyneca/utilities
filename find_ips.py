e1d86526ddb36d1e6a60fe8164fa211fc43e16e5
Luke Tuthill <lukegithub@gmail.com>
2016-02-15 13:39:39 +1100

    squished more bugs

--- find_ips.py
+++ find_ips.py
@@ -1,17 +1,22 @@
 import os
 import re
-if not os.path.exists('out.txt'): open('out.txt', 'x').close()
+
+if not os.path.exists('out.txt'):
+    open('out.txt', 'x').close()
+
 with open('out.txt', 'w') as out_file:
     for file in os.listdir():
         temp_list = []
         if not os.path.isdir(file):
             with open(file) as f:
+                i = 0
                 for line in f:
+                    i += 1
                     matches = re.findall(r'\d+\.\d+\.\d+\.\d+', line)
                     if len(matches) > 0:
-                        print(line)
-                        temp_list.append(line)
-            if len(temp_list) > 0:
-                out_file.write('File ' + file + '\n')
-                for l in temp_list:
-                    out_file.write('    ' + l + '\n')
+                        print(line.strip())
+                        temp_list.append((line.strip(), i))
+        if len(temp_list) > 0:
+            out_file.write('File ' + file + '\n')
+            for l in temp_list:
+                out_file.write('Line ' + str(l[1]) + ': ' + l[0] + '\n')
