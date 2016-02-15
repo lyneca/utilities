4fbad13c64b86c079ce88f9f77685bbc5ba31e7f
Luke Tuthill <lukegithub@gmail.com>
2016-02-15 13:27:43 +1100

    added a tool that extracts lines that contain an IP address out of every folder in the directory

---
+++ find_ips.py
@@ -0,0 +1,17 @@
+import os
+import re
+
+with open('out.txt', 'w') as out_file:
+    for file in os.listdir():
+        temp_list = []
+        if not os.path.isdir(file):
+            with open(file) as f:
+                for line in f:
+                    matches = re.findall(r'\d+\.\d+\.\d+\.\d+', line)
+                    if len(matches) > 0:
+                        print(line)
+                        temp_list.append(line)
+            if len(temp_list) > 0:
+                out_file.write('File ' + file + '\n')
+                for l in temp_list:
+                    out_file.write('    ' + l + '\n')
