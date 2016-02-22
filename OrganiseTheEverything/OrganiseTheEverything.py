3074abde8f4a0170a25049c0eec2199163b7e1ef
Luke Tuthill <lukegithub@gmail.com>
2016-02-23 10:59:10 +1100

    tidied up

--- OrganiseTheEverything/OrganiseTheEverything.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'wing2048'
-from packages.underscore import underscore
-from packages.file_type_organiser import organise
-underscore()
-organise()
--- OrganiseTheEverything/packages/underscore.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'wing2048'
-import os
-
-def underscore():
-    for file in os.listdir('.'):
-        os.rename(file, file.replace(' ', '_'))
